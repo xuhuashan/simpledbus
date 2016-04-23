6ff0e911b95c10f907674e89c7c968209ffcb56c
Watson Xu <xuhuashan@gmail.com>
2016-04-23 13:23:48 +0800

    add int64/uint64/signature/struct/dict_entry argument support for a dbus call

--- add.c
+++ add.c
@@ -118,6 +118,28 @@ static enum add_return add_uint32(lua_State *L, int index,
 	return ADD_OK;
 }
 
+static enum add_return add_int64(lua_State *L, int index,
+		DBusSignatureIter *type, DBusMessageIter *args)
+{
+	dbus_int64_t n;
+	if (!lua_isnumber(L, index))
+		return add_error(L, index, LUA_TNUMBER);
+	n = (dbus_int64_t)lua_tonumber(L, index);
+	dbus_message_iter_append_basic(args, DBUS_TYPE_INT64, &n);
+	return ADD_OK;
+}
+
+static enum add_return add_uint64(lua_State *L, int index,
+		DBusSignatureIter *type, DBusMessageIter *args)
+{
+	dbus_uint64_t n;
+	if (!lua_isnumber(L, index))
+		return add_error(L, index, LUA_TNUMBER);
+	n = (dbus_uint64_t)lua_tonumber(L, index);
+	dbus_message_iter_append_basic(args, DBUS_TYPE_UINT64, &n);
+	return ADD_OK;
+}
+
 static enum add_return add_double(lua_State *L, int index,
 		DBusSignatureIter *type, DBusMessageIter *args)
 {
@@ -151,6 +173,92 @@ static enum add_return add_object_path(lua_State *L, int index,
 	return ADD_OK;
 }
 
+static enum add_return add_signature(lua_State *L, int index,
+		DBusSignatureIter *type, DBusMessageIter *args)
+{
+	const char *s;
+	if (!lua_isstring(L, index))
+		return add_error(L, index, LUA_TSTRING);
+	s = lua_tostring(L, index);
+	if (!dbus_signature_validate(s, NULL)) {
+		lua_pushfstring(L, "(%s is is not a valid signature)", s);
+		return ADD_ERROR;
+	}
+	dbus_message_iter_append_basic(args, DBUS_TYPE_SIGNATURE, &s);
+	return ADD_OK;
+}
+
+static enum add_return add_dict_entry(lua_State *L, int index,
+		DBusSignatureIter *type, DBusMessageIter *args)
+{
+	DBusSignatureIter array_type;
+	DBusSignatureIter dict_type;
+	DBusMessageIter array_args;
+	DBusMessageIter dict_args;
+	char *signature;
+	add_function kf;
+	add_function vf;
+
+	if (!lua_istable(L, index))
+		return add_error(L, index, LUA_TTABLE);
+
+	dbus_signature_iter_recurse(type, &array_type);
+
+	signature = dbus_signature_iter_get_signature(&array_type);
+
+	dbus_signature_iter_recurse(&array_type, &dict_type);
+
+	if (!dbus_type_is_basic(
+		dbus_signature_iter_get_current_type(&dict_type))) {
+		lua_pushstring(L, "(DICT_ENTRY key must be a basic type)");
+	}
+	kf = get_addfunc(&dict_type);
+	if (!dbus_signature_iter_next(&dict_type)) {
+		lua_pushfstring(L, "(DICT_ENTRY must be key-value pair)");
+		return ADD_ERROR;
+	}
+	vf = get_addfunc(&dict_type);
+	if (dbus_signature_iter_next(&dict_type)) {
+		lua_pushstring(L, "(DICT_ENTRY must be key-value pair)");
+		return ADD_ERROR;
+	}
+
+	dbus_message_iter_open_container(args, DBUS_TYPE_ARRAY,
+			signature, &array_args);
+
+	lua_pushnil(L);
+	while (lua_next(L, index)) {
+		dbus_message_iter_open_container(&array_args, DBUS_TYPE_DICT_ENTRY,
+				NULL, &dict_args);
+
+		dbus_signature_iter_recurse(&array_type, &dict_type);
+		int top = lua_gettop(L);
+		if (kf(L, top - 1, &dict_type, &dict_args) != ADD_OK) {
+			lua_insert(L, -2);
+			lua_pop(L, 1);
+			return ADD_ERROR;
+		}
+		dbus_signature_iter_next(&dict_type);
+		if (vf(L, top, &dict_type, &dict_args) != ADD_OK) {
+			lua_insert(L, -2);
+			lua_pop(L, 1);
+			return ADD_ERROR;
+		}
+
+		lua_pop(L, 1);
+
+		dbus_message_iter_close_container(&array_args, &dict_args);
+	}
+
+	lua_pop(L, 1);
+
+	dbus_free(signature);
+
+	dbus_message_iter_close_container(args, &array_args);
+
+	return ADD_OK;
+}
+
 static enum add_return add_array(lua_State *L, int index,
 		DBusSignatureIter *type, DBusMessageIter *args)
 {
@@ -159,6 +267,11 @@ static enum add_return add_array(lua_State *L, int index,
 	char *signature;
 	add_function af;
 	int i;
+
+	if (dbus_signature_iter_get_element_type(type) ==
+					DBUS_TYPE_DICT_ENTRY) {
+		return add_dict_entry(L, index, type, args);
+	}
 
 	if (!lua_istable(L, index))
 		return add_error(L, index, LUA_TTABLE);
@@ -175,10 +288,13 @@ static enum add_return add_array(lua_State *L, int index,
 	i = 1;
 	while (1) {
 		lua_rawgeti(L, index, i);
-		if (lua_isnil(L, -1))
+		if (lua_isnil(L, -1)) {
+			lua_pop(L, 1);
 			break;
+		}
 
-		if (af(L, -1, &array_type, &array_args) != ADD_OK) {
+		if (af(L, lua_gettop(L), &array_type, &array_args) != ADD_OK) {
+			dbus_free(signature);
 			lua_insert(L, -2);
 			lua_pop(L, 1);
 			return ADD_ERROR;
@@ -189,11 +305,52 @@ static enum add_return add_array(lua_State *L, int index,
 		i++;
 	}
 
-	lua_pop(L, 1);
-
 	dbus_free(signature);
 
 	dbus_message_iter_close_container(args, &array_args);
+
+	return ADD_OK;
+}
+
+static enum add_return add_struct(lua_State *L, int index,
+		DBusSignatureIter *type, DBusMessageIter *args)
+{
+	DBusSignatureIter struct_type;
+	DBusMessageIter struct_args;
+	char *signature;
+	add_function af;
+	int i;
+
+	if (!lua_istable(L, index))
+		return add_error(L, index, LUA_TTABLE);
+
+	dbus_signature_iter_recurse(type, &struct_type);
+
+	signature = dbus_signature_iter_get_signature(&struct_type);
+
+	dbus_message_iter_open_container(args, DBUS_TYPE_STRUCT,
+			NULL, &struct_args);
+
+	i = 1;
+	do {
+		lua_rawgeti(L, index, i);
+
+		af = get_addfunc(&struct_type);
+		if (af(L, lua_gettop(L), &struct_type, &struct_args) != ADD_OK) {
+			dbus_free(signature);
+			lua_insert(L, -2);
+			lua_pop(L, 1);
+			return ADD_ERROR;
+		}
+
+		lua_pop(L, 1);
+
+		i++;
+	} while (dbus_signature_iter_next(&struct_type));
+
+	dbus_free(signature);
+
+	dbus_message_iter_close_container(args, &struct_args);
 
 	return ADD_OK;
 }
@@ -212,7 +369,7 @@ static enum add_return add_variant(lua_State *L, int index,
 	signature = lua_tostring(L, -1);
 	if (!signature || !dbus_signature_validate_single(signature, NULL)) {
 		lua_pop(L, 1);
-		lua_pushstring(L, "(variant has no valid signature)");
+		lua_pushstring(L, "(invalid variant signature)");
 		return ADD_ERROR;
 	}
 
@@ -254,14 +411,22 @@ static add_function get_addfunc(DBusSignatureIter *type)
 		return add_int32;
 	case DBUS_TYPE_UINT32:
 		return add_uint32;
+	case DBUS_TYPE_INT64:
+		return add_int64;
+	case DBUS_TYPE_UINT64:
+		return add_uint64;
 	case DBUS_TYPE_DOUBLE:
 		return add_double;
 	case DBUS_TYPE_STRING:
 		return add_string;
 	case DBUS_TYPE_OBJECT_PATH:
 		return add_object_path;
+	case DBUS_TYPE_SIGNATURE:
+		return add_signature;
 	case DBUS_TYPE_ARRAY:
 		return add_array;
+	case DBUS_TYPE_STRUCT:
+		return add_struct;
 	case DBUS_TYPE_VARIANT:
 		return add_variant;
 	}
@@ -278,6 +443,13 @@ EXPORT unsigned int add_arguments(lua_State *L, int start, int argc,
 
 	dbus_message_iter_init_append(msg, &args);
 	dbus_signature_iter_init(&type, signature);
+
+	if (!dbus_signature_validate(signature, NULL)) {
+		lua_pushfstring(L, "type error adding value #%d "
+					"of '%s' (invalid signature)",
+					i - start + 1, signature);
+		return 1;
+	}
 
 	do {
 		if (i > argc) {
